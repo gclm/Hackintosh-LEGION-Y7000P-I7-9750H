fefd020dc1f699a0f811d8303409edcce9e7746d
gclm <1719982754@qq.com>
2019-12-10 09:43:30 +0800

    整理代码

--- clear.sh
+++ clear.sh
@@ -2,3 +2,11 @@
 
  git rm -r --cache "EFI/APPLE"
  git rm -r --cache "EFI/Microsoft"
+ git rm -r --cache "$RECYCLE.BIN"
+ git rm -r --cache ".PKInstallSandboxManager"
+ git rm -r --cache ".Spotlight-V100"
+ git rm -r --cache ".TemporaryItems"
+ git rm -r --cache ".Trashes"
+ git rm -r --cache "._README.md"
+ git rm -r --cache ".fseventsd"
+
